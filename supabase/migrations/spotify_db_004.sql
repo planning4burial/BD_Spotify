d40411a7f01bb33826dfe892bb2a427cdfcc70e3
jean <741jean@gmail.com>
2026-07-09 11:59:49 -0300

    tipo de plano criado

--- supabase/migrations/spotify_db_004.sql
+++ supabase/migrations/spotify_db_004.sql
@@ -5,3 +5,11 @@ create type spotify_plan as enum (
   'family_premium',
   'duo_premium'
 );
+
+CREATE type spotify_plan as enum (
+  'free',
+  'student_premium',
+  'standart_premium',
+  'family_premium',
+  'duo_premium'
+);

c92944f49c59e3b024961533eeb56b35074456ce
jean <741jean@gmail.com>
2026-07-09 12:11:19 -0300

    criado status possiveis da solicitacao de playlist blend

--- supabase/migrations/spotify_db_004.sql
+++ supabase/migrations/spotify_db_004.sql
@@ -6,10 +6,8 @@ create type spotify_plan as enum (
   'duo_premium'
 );
 
-CREATE type spotify_plan as enum (
-  'free',
-  'student_premium',
-  'standart_premium',
-  'family_premium',
-  'duo_premium'
+CREATE TYPE public.playlist_match_status AS ENUM (
+  'active',
+  'expired',
+  'disbanded'
 );
